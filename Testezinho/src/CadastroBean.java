77097421f3f8a700ef533cc532c97f8fbe15d9ed
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-03 09:41:03 -0300

    JavaServerFaces

--- Testezinho/src/CadastroBean.java
+++ Testezinho/src/CadastroBean.java
@@ -2,6 +2,39 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class CadastroBean {
+	private String nome;
+	private String cpf;
+	private int idade;
+	
+	
+	
+	
+	public void cadastrar() {}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
 	
 	
 }
